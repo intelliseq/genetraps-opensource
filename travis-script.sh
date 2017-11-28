198e917503b75bbc9db651b62efdcef19832caa1
Marcin Piechota <piechota.marcin@gmail.com>
2017-11-28 15:29:57 +0100

    travis-init-7

--- travis-script.sh
+++ travis-script.sh
@@ -1,5 +1,5 @@
 echo "Running genetraps"
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure --profile kms
-echo `aws kms describe-aliases`
+echo `aws kms list-aliases`
 exit 0
