c028babea4a378c0ffe9d27a40c42b929665b5a1
Marcin Piechota <piechota.marcin@gmail.com>
2017-11-29 23:11:53 +0100

    dx 2

---
+++ api-dx-touch/src/main/java/pl/intelliseq/genetraps/api/dx/touch/TouchApplication.java
@@ -0,0 +1,12 @@
+package pl.intelliseq.genetraps.api.dx.touch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouchApplication.class, args);
+	}
+}
--- travis-script.sh
+++ travis-script.sh
@@ -1,7 +1,7 @@
 echo "Running genetraps"
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure --profile kms
-. scripts/get-secrets.sh
+. scripts/get-secrets.sh kms-store
 dx login --token $DNANEXUS_TOKEN --noprojects
 echo `dx ls`
 exit 0
