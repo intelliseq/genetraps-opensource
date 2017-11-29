958c9452ffd3375f350ef912974820791942944a
Marcin Piechota <piechota.marcin@gmail.com>
2017-11-29 23:21:47 +0100

    dx 3

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -3,9 +3,11 @@
 # use . ./get-secrets.sh kms-store 
 
 BUCKET=$1
+echo "Retrieving keys"
 
 for KEY in `aws s3 ls s3://$BUCKET | tr -s ' ' | cut -d " " -f 4`
 do
+    echo $KEY
     VALUE=`aws kms decrypt --ciphertext-blob fileb://<(aws s3 cp s3://kms-store/$KEY -) --output text --query Plaintext | base64 -d`
     eval "$KEY=$VALUE"
 done
--- travis-script.sh
+++ travis-script.sh
@@ -1,7 +1,10 @@
 echo "Running genetraps"
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure
 #printf $AWS_ACCESS_KEY_ID"\n"$AWS_SECRET_ACCESS_KEY"\n"$AWS_REGION"\njson" | aws configure --profile kms
+echo "Getting scripts"
+echo "token"`echo $DNANEXUS_TOKEN | cut -c1-5`
 . scripts/get-secrets.sh kms-store
-dx login --token $DNANEXUS_TOKEN --noprojects
+echo "token"`echo $DNANEXUS_TOKEN | cut -c1-5`
+#dx login --token $DNANEXUS_TOKEN --noprojects
 echo `dx ls`
 exit 0
