77a65bdce27481f47204acf918c3ab9d6387d300
Kamil Antonczak <kamil.m.antonczak@gmail.com>
2017-12-06 14:25:49 +0100

    fix to works on Mac

--- scripts/get-secrets.sh
+++ scripts/get-secrets.sh
@@ -5,9 +5,16 @@
 BUCKET=$1
 echo "Retrieving keys"
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     o=d;;
+    Darwin*)    o=D;;
+esac
+
+
 for KEY in `aws s3 ls s3://$BUCKET | tr -s ' ' | cut -d " " -f 4`
 do
     echo $KEY
-    VALUE=`aws kms decrypt --ciphertext-blob fileb://<(aws s3 cp s3://kms-store/$KEY -) --output text --query Plaintext | base64 -d`
+    VALUE=`aws kms decrypt --ciphertext-blob fileb://<(aws s3 cp s3://kms-store/$KEY -) --output text --query Plaintext | base64 -$o`
     eval "$KEY=$VALUE"
 done
