e8d45c51dd9a1b444038cc06e1efa57eda4ce7e1
Marcin Piechota <piechota.marcin@gmail.com>
2018-01-31 13:39:17 +0100

    client-dnatoken

--- travis-script.sh
+++ travis-script.sh
@@ -30,22 +30,37 @@ check
 echo $LOG_PREFIX $LOG_APP "testing..."
 echo $(curl localhost:8080/touch)
 
-### BUILDING CLINET_EXPLORARE ###
+### BUILDING CLIENT_EXPLORARE ###
 LOG_APP="client-explorare: "
 echo $LOG_PREFIX $LOG_APP "compiling..."
 mkdir ./client-explorare/dist
 npm --prefix ./client-explorare install ./client-explorare
 echo $LOG_PREFIX $LOG_APP "testing..."
 npm --prefix client-explorare run test
-echo $LOG_PREFIX $LOG_APP "client-explorare: dockerizing..."
+echo $LOG_PREFIX $LOG_APP "dockerizing..."
 docker build -t pl.intelliseq.genetraps.client.explorare/client-explorare:latest client-explorare
-echo $LOG_PREFIX $LOG_APP "client-explorare: running docker..."
+echo $LOG_PREFIX $LOG_APP "running docker..."
 docker run -d -p 8081:8081 pl.intelliseq.genetraps.client.explorare/client-explorare:latest
-echo $LOG_PREFIX $LOG_APP "client-explorare: waiting for service..."
+echo $LOG_PREFIX $LOG_APP "waiting for service..."
 ./scripts/wait-for-service.sh localhost:8081/hello 60
-echo $LOG_PREFIX $LOG_APP "client-explorare: checking for error..."
+echo $LOG_PREFIX $LOG_APP "checking for error..."
 check
 
+### BUILDING CLIENT_DNATOKEN ###
+LOG_APP="client-dnatoken: "
+echo $LOG_PREFIX $LOG_APP "compiling..."
+mkdir ./client-dnatoken/dist
+npm --prefix ./client-dnatoken install ./client-dnatoken
+echo $LOG_PREFIX $LOG_APP "testing..."
+npm --prefix client-dnatoken run test
+echo $LOG_PREFIX $LOG_APP "dockerizing..."
+docker build -t pl.intelliseq.genetraps.client.dnatoken/client-dnatoken:latest client-dnatoken
+echo $LOG_PREFIX $LOG_APP "running docker..."
+docker run -d -p 8083:8083 pl.intelliseq.genetraps.client.dnatoken/client-dnatoken:latest
+echo $LOG_PREFIX $LOG_APP "waiting for service..."
+./scripts/wait-for-service.sh localhost:8083/hello 60
+echo $LOG_PREFIX $LOG_APP "checking for error..."
+check
 
 echo $LOG_PREFIX "pushing to AWS"
 ecs-cli configure profile --profile-name genetraps --access-key $ECS_CLI_GENETRAPS_DEV_KEY_ID --secret-key $ECS_CLI_GENETRAPS_DEV_ACCESS_KEY
@@ -64,20 +79,28 @@ echo `ecs-cli ps --aws-profile genetraps`
 
 TAG_API_DX=`ls -alR --full-time api-dx/ -Ibin -Ibuild -I.* | sha1sum | cut -d " " -f1`
 TAG_CLIENT_EXPLORARE=`ls -alR --full-time client-explorare/ -Inode_modules -Idist -Ietc -I.* | sha1sum | cut -d " " -f1`
+TAG_CLIENT_DNATOKEN=`ls -alR --full-time client-dnatoken/ -Inode_modules -Idist -Ietc -I.* | sha1sum | cut -d " " -f1`
 #echo $TAG_API_DX
 #echo $TAG_CLIENT_EXPLORARE
 IMAGE_API_DX=pl.intelliseq.genetraps.api.dx/api-dx:latest
 IMAGE_CLIENT_EXPLORARE=pl.intelliseq.genetraps.client.explorare/client-explorare:latest
+IMAGE_CLIENT_DNATOKEN=pl.intelliseq.genetraps.client.dnatoken/client-dnatoken:latest
 #echo $IMAGE_CLIENT_EXPLORARE
 TAG_API_DX=$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/genetraps-api-dx:"$TAG_API_DX
 TAG_CLIENT_EXPLORARE=$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/genetraps-client-explorare:"$TAG_CLIENT_EXPLORARE
+TAG_CLIENT_DNATOKEN=$AWS_ACCOUNT_ID".dkr.ecr.us-east-1.amazonaws.com/genetraps-client-dnatoken:"$TAG_CLIENT_DNATOKEN
 docker tag $IMAGE_API_DX $TAG_API_DX
 docker tag $IMAGE_CLIENT_EXPLORARE $TAG_CLIENT_EXPLORARE
+docker tag $IMAGE_CLIENT_DNATOKEN $TAG_CLIENT_DNATOKEN
 ecs-cli push $TAG_API_DX --ecs-profile genetraps
 ecs-cli push $TAG_CLIENT_EXPLORARE --ecs-profile genetraps
+ecs-cli push $TAG_CLIENT_DNATOKEN --ecs-profile genetraps
 #echo $TAG_CLIENT_EXPLORARE 
 cat aws-conf/docker-compose-template.yml | sed 's@clientExplorareImageTag@'"$TAG_CLIENT_EXPLORARE"'@' > docker-compose.yml
 ecs-cli compose --project-name genetraps-client-explorare -f docker-compose.yml --ecs-params ./aws-conf/ecs-params.yml service up --target-group-arn "arn:aws:elasticloadbalancing:us-east-1:"$AWS_ACCOUNT_ID":targetgroup/genetraps-explorare-client/"$AWS_GENETRAPS_TARGET_GROUP --container-name client-explorare --container-port 8081 --ecs-profile genetraps
+cat aws-conf/docker-compose-template.yml | sed 's@clientDnatokenImageTag@'"$TAG_CLIENT_DNATOKEN"'@' > docker-compose.yml
+ecs-cli compose --project-name genetraps-client-dnatoken -f docker-compose.yml --ecs-params ./aws-conf/ecs-params.yml service up --target-group-arn "arn:aws:elasticloadbalancing:us-east-1:"$AWS_ACCOUNT_ID":targetgroup/genetraps-dnatoken-client/"$AWS_GENETRAPS_TARGET_GROUP --container-name client-
+lient-dnatoken --container-port 8083 --ecs-profile genetraps
 
 #./scripts/update-repo.sh
 
