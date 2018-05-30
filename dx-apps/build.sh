f4832d9ac4a5fd6134f0e0babaae21b0dea2b81a
marpiech <piechota.marcin@gmail.com>
2018-05-30 09:33:46 +0200

    removed iseq-apps

--- dx-apps/build.sh
+++ dx-apps/build.sh
@@ -1,3 +1,7 @@
+### prepare tools
+if [ ! -f /tmp/dxWDL/dxWDL-0.65.jar ]; then
+    wget https://github.com/dnanexus/dxWDL/releases/download/0.65/dxWDL-0.65.jar --directory-prefix /tmp/dxWDL
+fi
 ### login
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
 ### build docker
