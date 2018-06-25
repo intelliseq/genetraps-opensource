f77713ff35c23479e6b7e8790da8cc5aab117a49
marpiech <piechota.marcin@gmail.com>
2018-06-25 17:00:34 +0200

    ecs-cli for client index bug #1

--- travis-script.sh
+++ travis-script.sh
@@ -29,11 +29,12 @@ echo $LOG_PREFIX "logging to ecr..."
 ### BUILDING CLIENT_INDEX ###
 LOG_APP="client-index: "
 echo $LOG_PREFIX $LOG_APP "setting tag"
-CLIENT_INDEX_TAG=$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/genetraps-client-index"
 CLIENT_INDEX_CHECKSUM=`find client-index -type f -exec md5sum {} \; | sort -k 2 | md5sum | sed 's/  -//g'`
-echo $LOG_PREFIX $LOG_APP $CLIENT_INDEX_TAG":"$CLIENT_INDEX_CHECKSUM
-docker build client-index/ -t $CLIENT_INDEX_TAG":"$CLIENT_INDEX_CHECKSUM
-docker push $CLIENT_INDEX_TAG":"$CLIENT_INDEX_CHECKSUM
+CLIENT_INDEX_TAG=$AWS_ACCOUNT_ID".dkr.ecr."$AWS_REGION".amazonaws.com/genetraps-client-index:"$CLIENT_INDEX_CHECKSUM
+echo $LOG_PREFIX $LOG_APP $CLIENT_INDEX_TAG
+docker build client-index/ -t $CLIENT_INDEX_TAG
+docker push $CLIENT_INDEX_TAG
+cat aws-conf/docker-compose-template.yml | sed 's@clientIndexImageTag@'"$CLIENT_INDEX_TAG"'@' > docker-compose.yml
 ecs-cli compose --project-name genetraps-client-index -f docker-compose.yml --ecs-params ./aws-conf/ecs-params.yml service up --target-group-arn "arn:aws:elasticloadbalancing:"$AWS_REGION":"$AWS_ACCOUNTID":targetgroup/client-index-target-group/"$ECS_CLI_TG_CLIENT_INDEX --container-name client-index --container-port $ECS_CLI_PORT_CLIENT_INDEX --aws-profile genetraps
 
 ### BUILDING API_DX ###
