c7ba5d645d3e343aa8e8f50a7ae68856a36a4856
marpiech <piechota.marcin@gmail.com>
2018-07-25 13:45:20 +0200

    client-explorare

--- client-explorare/app/app.config.js
+++ client-explorare/app/app.config.js
@@ -3,6 +3,6 @@ var core_1 = require("@angular/core");
 exports.APP_CONFIG = new core_1.OpaqueToken("app.config");
 exports.AppConfig = {
     //apiEndpoint: "http://localhost:8080"
-    apiEndpoint: "http://178.216.201.71:8080"
+    apiEndpoint: "http://genetraps.intelliseq.pl:8082"
 };
 //# sourceMappingURL=app.config.js.map
