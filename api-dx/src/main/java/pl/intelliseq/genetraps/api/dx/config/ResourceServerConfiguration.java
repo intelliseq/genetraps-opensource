a0d841ba148a33bfd5c2ed924c0078644940e195
Z-DNA <kamil.m.antonczak@gmail.com>
2018-08-06 09:39:02 +0200

    Logging

--- api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/config/ResourceServerConfiguration.java
+++ api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/config/ResourceServerConfiguration.java
@@ -1,5 +1,6 @@
 package pl.intelliseq.genetraps.api.dx.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +12,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 
 @Configuration
 @EnableResourceServer
+@Log4j2
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        log.info("Profile: Main");
         http
                 .csrf().disable()
                 .authorizeRequests()
--- api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/config/ResourceServerConfigurationForTest.java
+++ api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/config/ResourceServerConfigurationForTest.java
@@ -1,5 +1,6 @@
 package pl.intelliseq.genetraps.api.dx.config;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,10 +14,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenCo
 @Configuration
 @EnableResourceServer
 @Profile("test")
+@Log4j2
 public class ResourceServerConfigurationForTest extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        log.info("Profile: Test");
         http
                 .csrf().disable()
                 .authorizeRequests()
