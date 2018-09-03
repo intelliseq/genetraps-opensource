c7b15af700b89b693f9fc24941405536529d77e2
Kaleeth <gregor.16.waw@gmail.com>
2018-09-03 10:55:20 +0200

    api-security - preflight CORS bean

--- api-security/src/main/java/pl/intelliseq/genetraps/api/security/BeansConfiguration.java
+++ api-security/src/main/java/pl/intelliseq/genetraps/api/security/BeansConfiguration.java
@@ -1,10 +1,15 @@
 package pl.intelliseq.genetraps.api.security;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.sql.DataSource;
 
@@ -27,5 +32,19 @@ public class BeansConfiguration {
     @Bean
     AuroraDBManager auroraDBManager(){
         return new AuroraDBManager();
+    }
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
     }
 }
