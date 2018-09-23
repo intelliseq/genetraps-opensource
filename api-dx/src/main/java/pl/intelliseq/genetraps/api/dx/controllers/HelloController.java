a871ce6ec6ba2b0160623a150a3b0e47d6e62ae6
Marcin Piechota <piechota.marcin@gmail.com>
2018-09-23 16:24:42 +0200

    Update HelloController.java

--- api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/controllers/HelloController.java
+++ api-dx/src/main/java/pl/intelliseq/genetraps/api/dx/controllers/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 
 @RestController
 public class HelloController {
