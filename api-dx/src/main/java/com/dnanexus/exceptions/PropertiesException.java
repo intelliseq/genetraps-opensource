3f5c11c599d8c0e2cd2188e8762a9019f447038e
Kaleeth <gregor.16.waw@gmail.com>
2018-10-12 10:46:54 +0200

    PipelineController and PropertiesException

---
+++ api-dx/src/main/java/com/dnanexus/exceptions/PropertiesException.java
@@ -0,0 +1,16 @@
+package com.dnanexus.exceptions;
+
+/**
+ * A problem with existence of 'properties' file (specified in message) has occured
+ */
+public class PropertiesException extends RuntimeException {
+
+    public PropertiesException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() { return "The 'properties' file: " + this.getMessage(); }
+
+    private static final long serialVersionUID = 2376890343328017594L;
+}
